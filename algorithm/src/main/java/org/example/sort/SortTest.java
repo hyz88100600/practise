5660bc773ca722a452b2fff0807bce2a9639f682
hyz <hyz@test.com>
2020-06-02 19:00:25 +0800

    add some class

---
+++ algorithm/src/main/java/org/example/sort/SortTest.java
@@ -0,0 +1,17 @@
+package org.example.sort;
+
+import java.util.Arrays;
+
+/*
+ *@author hanyunzhuo
+ *@date 2020/6/2 17:43
+ */
+public class SortTest {
+
+    public static void main(String[] args) {
+        int[] arr = {6,1,2,7,9,3,4,5,10,8};
+        QuickSort.quickSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
