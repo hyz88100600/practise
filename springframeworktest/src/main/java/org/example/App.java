dd772dc0a8dcd334e0d60c53110fe4ca109caef7
hyz <hyz@test.com>
2020-06-22 15:36:14 +0800

    add some class

---
+++ springframeworktest/src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Hello world!
+ *1
+ */
+public class App extends Parent
+{
+
+    static int a = 2;
+  //  String s1 = "b";
+    String s2 = new String("b");
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ springframeworktest/src/main/java/org/example/Parent.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/*
+ *@author hanyunzhuo
+ *@date 2020/6/19 16:04
+ */
+public class Parent {
+
+    String ss = "ss";
+}
